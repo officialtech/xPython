e52c3b5cbc6ae8328bc0f30ba545416f5d22d1dd
jb <46815338+officialtech@users.noreply.github.com>
2019-02-12 23:30:34 +0530

    Update static_variable | xpython.py

--- static_variable | xpython.py
+++ static_variable | xpython.py
@@ -1,14 +1,14 @@
-********************************************** STATIC VARIABLES *****************************************
+**********************************************# STATIC VARIABLES *****************************************
 
-The variables which are declared inside the class and outside the 'method' are called static variable.
+# The variables which are declared inside the class and outside the 'method' are called static variable.
 
-Static variables will holds common values for every object.
+# Static variables will holds common values for every object.
 
-Static variables will get memory for one time.
+# Static variables will get memory for one time.
 
-To call static variables we use class name.
+# To call static variables we use class name.
 
-Static variable will get memory at class loading time.
+# Static variable will get memory at class loading time.
 
 
 
@@ -41,24 +41,69 @@ def function():
 function()
 
 **********************************************************************************************************************
-                     REMEMBER BELOW WE ARE CREATING TWO 'PYTHON FILES' OR MODULES
+                     # REMEMBER BELOW WE ARE CREATING TWO 'PYTHON FILES' OR MODULES
 **********************************************************************************************************************
 
 # Import a class to another class
 
---------------------step 1: create a module, like
+--------------------# step 1: create a module, like
 
 class Employee:
     name = "official tech"
     ctn = 1010
----------------------step 2: save as whatever.py
+---------------------# step 2: save as whatever.py
 
----------------------step 3: create annother module, like
+---------------------# step 3: create annother module, like
 
 from whatever import Employee
 print(Employee.name)
 print(Employee.ctn)
 
----------------------step 4: save as what_so_ever.py and run
+---------------------# step 4: save as what_so_ever.py and run
 
+
+----------------------------------#  Program on instance and static variable  ----------------------------------------------
+
+
+class Official_tech:
+    # static block start
+    
+    comp_name = 'official tech Inc.'
+    comp_c_no = 10010101000010
+    
+    # static block ends
+    
+    def team_member(self, id, name, salary = 00.00):   # Remember this is a method, not a function
+        # Instance block
+        
+        self.i_d = id
+        self.n_m = name
+        self.sal = salary
+        
+    def mess_disp(self):
+        print(Official_tech.comp_c_no) # printing Static variable using Class name
+        
+        print(self.i_d)  #  printing instance variable 
+        print(self.sal)  
+        print(self.n_m)
+        
+        print(Official_tech.comp_name)
+        
+
+obj_var = Official_tech()  # 1.creating object and storing to variable
+
+obj_var.team_member(808, 'tppa', 400000.00) # giving arg's to parameters
+obj_var.mess_disp()
+print("~ " * 10)
+
+obj_var1 = Official_tech()  # 2.creating another object and storing to variable
+obj_var1.team_member(1010, 'sqst', 350000.00)
+obj_var.mess_disp()
+print("~ " * 10)
+
+print(obj_var.comp_name)  # calling static variable
+print("~ " * 10)
+
+print(obj_var1.i_d)  # calling Instance variable
+print(obj_var1.comp_c_no)
 
